6ae359deac5dee7f7f3d36ad3408879bb836acc1
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2024-04-06 15:44:15 +0200

    setiing background

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet,ImageBackground } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-<StartGameScreen/>
+    <LinearGradient
+      colors={['#940764','#ddb52f']}
+      style={styles.rootScreen}
+      >
+      <ImageBackground
+       // @ts-ignore
+       source={require('./assets/images/background.png')}
+       resizeMode="cover"
+       style={styles.rootScreen}
+imageStyle={styles.bgImage}
+>
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  rootScreen: {
+    flex: 1,
+  },
+  bgImage:{
+    opacity:0.15
+  }
 });
